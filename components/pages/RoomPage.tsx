082a7e80a5885507688e7fa3fda210ed2fcc3a73
takeru <takeruchandesu@gmail.com>
2026-02-08 14:23:43 +0900

    fix

--- components/pages/RoomPage.tsx
+++ components/pages/RoomPage.tsx
@@ -191,9 +191,9 @@ export default function RoomPage({ title, shortId, scores }: { title: string, sh
                 whileInView={{ opacity: 1, x: 0 }}
                 viewport={{ once: true, amount: 0.3 }}
                 transition={{ type: 'spring', stiffness: 300, damping: 25, delay: index * 0.2 }}
-                className={`relative flex items-center justify-between p-2 shadow mb-2 transform skew-x-[-10deg] rounded-xl ${index === 0 || scores[index - 1] && scores[index - 1].point === score.point ? 'bg-yellow-400' : 'bg-yellow-400/50'}`}
+                className={`relative flex items-center justify-between p-2 shadow mb-2 transform skew-x-[-10deg] rounded-xl ${index === 0 || scores[index - 1] === 0 && scores[index - 1] ? 'bg-yellow-400' : 'bg-yellow-400/50'}`}
               >
-                {index === 0 || scores[index - 1] && scores[index - 1].point === score.point ? (
+                {index === 0 || scores[index - 1] === 0 ? (
                   <>
                     <div className='absolute top-0 left-12'>
                       <TbCrown size='1.3em' className='text-white drop-shadow-lg' />
